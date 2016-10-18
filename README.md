8f3684b9cefc69cb4cfb5f396720a28f1b747b94
John Hanusek <hanusek@gmail.com>
2016-10-17 23:20:13 -0700

    updated readme

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Batch Screenshots
 
 Using ImageMagick we batch a group of png filled directories into screenshots for the App Store
 
-First
+Begin
 - install ImageMagic via [This Link](http://www.gctv.ne.jp/~yokota/magick/#mem1)
 - change script permissions like this `chmod 700 ./sshot.sh`
 - run bash `./sshot.sh ~/Desktop/stickers/` where `~/Desktop/stickers/` is the directory with all your sticker directories
-- a directory will be add call `screenshots` and pngs will be added in sequential order ie. screenshot000.png, screenshot001.png, screenshot002.png ...
+- a directory will be added called `screenshots` and pngs will be added in sequential order ie. screenshot000.png, screenshot001.png, screenshot002.png ...
 
 Todo
-- place screenshots into created Screenshot directories
+- place screenshots into created Screenshot directories within sticker directories
 - include multiple versions of screenshots.
 
--- sshot.sh
+++ sshot.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+# create a count for sequential name
 count=0
+
+# add temp directory if it doesn't already exist
 if [ ! -d .screenshots/ ]; then
   mkdir -p .screenshots/;
 fi
+
+# iterate through included directories
 for d in "$1"*; do
+
+	# create empty array to store pngs
 	pngs=()
+
+	# add only pngs to the array
 	for f in "$d"/*.png; do
 		pngs+=("$f")
 	done
 	
+	# only itereate through pngs if there are more than 3
 	if (("${#pngs[@]}" >= 3))
 	then
 		echo 'processing directory: '$d
 
+		# for display purposes only
 		for (( i=1; i<=3; i++ ))
 		do
 		   echo $i " : " ${pngs[$i-1]}
 		done
 		#echo 'this is the count: '$count
 
+		# ImageMagick part, select the first three and add to included empty screen_shot.png
 		convert \
 			./screen_shot.png \
 			\( "${pngs[0]}" -resize 330x330 \) -geometry +375+887 -composite \
@@ -27,6 +39,7 @@ for d in "$1"*; do
 			\( "${pngs[2]}" -resize 330x330 \) -geometry +375+341 -composite \
 			./screenshots/screenshot00$count.png
 
+		# increment count
 		((count++))
 	fi
 	
